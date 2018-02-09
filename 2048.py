201e591a6fd9232d766e4efa6dfdde3703db6138
LTstrange <33590343+LTstrange@users.noreply.github.com>
2018-02-09 10:04:07 +0800

    Add files via upload

--- 2048.py
+++ 2048.py
@@ -1,6 +1,10 @@
 import random
 
 field=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+score = 0
+def restart():
+    global field
+    field=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
 
 def add_number():#随机选择添加数字
     number_type = random.randint(0,3)
@@ -30,51 +34,99 @@ def print_screen():#打印屏幕
     print(field[2])
     print(field[3])
 
-def operate():#决定输入操作
+def operates():#决定输入操作
     operate = input('whats your operate:')
     return operate
 
 def leftward(obj):#使field向左滑动
+    global score
     for ind,y_obj in enumerate(obj):
-        num_zero = obj.count(0)
+        print(y_obj)
+        num_zero = y_obj.count(0)
         for i in range(0,num_zero):
             obj[ind].remove(0)
             obj[ind].append(0)
         if y_obj[0] == y_obj[1]:
             obj[ind][0] = y_obj[0] + y_obj[1]
+            score += obj[ind][0]
             obj[ind][1] = obj[ind][2]
             obj[ind][2] = obj[ind][3]
             obj[ind][3] = 0
             if y_obj[1] == y_obj[2]:
                 obj[ind][1] = y_obj[1] +y_obj[2]
+                score += obj[ind][1]
                 obj[ind][2] = obj[ind][3]
                 obj[ind][3] = 0
         else:
             if y_obj[1] == y_obj[2]:
                 obj[ind][1] = y_obj[1] + y_obj[2]
+                score += obj[ind][1]
                 obj[ind][2] = y_obj[3]
                 obj[ind][3] = 0
             else:
                 if y_obj[2] == y_obj[3]:
                     obj[ind][2] = y_obj[2] + y_obj[3]
+                    score += obj[ind][2]
                     obj[ind][3] = 0
     return obj
 
-def Fl2r(obj):
+def Fl2r(obj):#转换field方向（左到右）
     for ind,y_obj in enumerate(obj):
         obj[ind] = y_obj.reverse()
     return obj
 
-def Fl2u(obj):
-    temp = obj
-    
+def Fl2u(obj):#转换field方向（左到上）
+    temp = list(zip(obj[0],obj[1],obj[2],obj[3]))
+    obj = []
+    for each in temp:
+        obj.extend([list(each)])
+    return obj
+        
+def restart():
+    global field
+    field=[[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
+    field = add_add(field)
+    field = add_add(field)
 
-score = 0
+def final_operate():
+    global field
+    while True:
+        operate = operates()
+        if operate == 'l':
+            field = leftward(field)
+            break
+        elif operate == 'u':
+            field = Fl2u(field)
+            field = leftward(field)
+            field = Fl2u(field)
+            break
+        elif operate == 'r':
+            field = Fl2r(field)
+            field = leftward(field)
+            field = Fl2r(field)
+            break
+        elif operate == 'd':
+            field = Fl2u(field)
+            field = Fl2r(field)
+            field = leftward(field)
+            field = Fl2r(field)
+            field = Fl2u(field)
+            break
+        else:
+            print('please input u,d,l,r')
+            continue
 
-
-field = add_add(field)
+#while True:
+restart()
+print_screen()
+#    num_zero = 0
+#    for each in field:
+#        zero_number += field.count(0)
+#    if zero_number = 0:
+#        break
+final_operate()
 field = add_add(field)
 print_screen()
-operate()
-field = leftward(field)
+final_operate()
+field = add_add(field)
 print_screen()
