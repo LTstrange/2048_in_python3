289fd79e0677ccbaa41e5a85c080cba8497a2797
LTstrange <33590343+LTstrange@users.noreply.github.com>
2018-02-27 01:18:06 +0800

    对程序进行详细的注释
    对程序进行详细的注释，并标注了bug可能位置。

--- _2048_.py
+++ _2048_.py
@@ -8,7 +8,9 @@ def version():
 
 def restart():#重置游戏(finished
     score = 0
+    #下一行生成4*4的全零矩阵
     field= [[0 for i in range(4)] for j in range(4)]
+    #添加两个随机数
     field = add_add(field)
     field = add_add(field)
     return field,score
@@ -21,13 +23,14 @@ def add_number():#随机选择添加数字(finished
         add_num = 2
     return add_num
 
-def add_add(obj,x_max=3,y_max=3):#添加数字(finished
-    zero_location =[]
+def add_add(obj,x_max=3,y_max=3):#选择位置并添加数字(finished
+    zero_location =[]#记录零所在的位置
     for y_ind,y_obj in enumerate(obj):
         for x_ind,x_obj in enumerate(y_obj):
             if x_obj == 0:
                 zero_location.extend([[x_ind,y_ind]])
-    location_ind=random.randint(0,len(zero_location)-1)
+    location_ind=random.randint(0,len(zero_location)-1)#用随机数确定位置
+    #下面一行是将add_number()生成的数字插入field
     obj[zero_location[location_ind][0]][zero_location[location_ind][1]] = add_number()
     return obj
 
@@ -47,9 +50,10 @@ def leftward(obj,score):#使field向左滑动(finished
     for ind,y_obj in enumerate(obj):
         num_zero = y_obj.count(0)
         for i in range(0,num_zero):#使数字全部滑到左侧，怀疑bug可能在此处
-            obj[ind].remove(0)
+            obj[ind].remove(0)#怀疑bug可能为我没查到的remove用法，可能性小
             obj[ind].append(0)
         #之后三个if都是使数字逐级相加合并，怀疑bug可能在此处
+        #bug可能为field的更新问题
         if y_obj[0] == y_obj[1]:
             obj[ind][0] = y_obj[0] + y_obj[1]
             score += obj[ind][0]
@@ -81,6 +85,7 @@ def Fl2u(obj):#转换field方向（左到上）(finished
     return obj
         
 def final_operate(field,score):#翻译操作符为具体操作函数(finished
+    #我认为bug不可能在此处
     while True:
         operate = operates()
         if operate == 'a':
@@ -116,12 +121,13 @@ def main(key=0):#主函数
         field,score = final_operate(field,score)
         if tmp != field:
             temp =copy.deepcopy(field)
-            #此处我怀疑为添加的数字将正常数字替换掉而导致bug
+            #上一行的deepcopy是我对避免bug的一个尝试，但未解决
+            #我怀疑为添加的数字将正常数字替换掉而导致bug
             field = add_add(temp)
             print_screen(field,score)
-            zero_number = 0
         else:
             print('This move has no efficient')
+        zero_number = 0#对field中的空位进行检测
         for each in field:
             zero_number = field.count(0)
         if zero_number != 0:
